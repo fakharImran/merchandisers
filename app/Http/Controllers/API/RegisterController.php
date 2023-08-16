d68f7cb697f516ba2f9eec90091171b24c4ad68d
fakharImran <fakhar.imran008@gmail.com>
2023-08-16 14:09:41 +0500

    fakharsetTImezone

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -68,7 +68,7 @@ class RegisterController extends BaseController
         $tempUser->company_id= $request->company_id;
         $tempUser->user_id=  $user->id;
         $tempUser->access_privilege= 'Active';
-        $tempUser->last_login_date_time=  date("Y-m-d H:i:s");
+        $tempUser->last_login_date_time=  date("Y-m-d h:i");
 
         $user->companyUser()->save($tempUser);
 
@@ -99,7 +99,7 @@ class RegisterController extends BaseController
                 $success['token'] = $user->createToken('api-token')->plainTextToken;
                 $success['name'] =  $user->name;
     
-                $user->companyUser->last_login_date_time =  date("Y-m-d H:i:s");
+                $user->companyUser->last_login_date_time =  date("Y-m-d h:i");
                 //update last log in date time for user 
                 $user->companyUser->save();
     
