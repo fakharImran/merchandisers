f44d7855de6b10a2d5dded6afd9fb22ff2eb8c40
fakharImran <fakhar.imran008@gmail.com>
2023-09-21 16:54:24 +0500

    fakhar udates

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import laravel from 'laravel-vite-plugin'; // Assuming this is the correct impor
 export default defineConfig({
     publicDir: 'public',
     base: '/public/', // Adjust this based on your asset path
-    outDir: 'public',
     plugins: [
         laravel({
             input: [
