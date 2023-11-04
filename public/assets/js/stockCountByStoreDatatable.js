c0df43d4020ed8a25a71f4b85155db7d6b5cd5fb
fakharImran <fakhar.imran008@gmail.com>
2023-11-04 17:52:40 +0500

    fakhar update

--- public/assets/js/stockCountByStoreDatatable.js
+++ public/assets/js/stockCountByStoreDatatable.js
@@ -16,7 +16,7 @@ function formatDateYMD(date) {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-
+    console.log(`${year}-${month}-${day}`);
     return `${year}-${month}-${day}`;
 }
 function setCards(table, startDate=0, endDate=0)
@@ -384,6 +384,7 @@ function changePeriod(e) {
         case 'Daily':
             
             createLastDaysDates(convertedToChartData);
+            console.log('labels, periodData)->>>', labels, periodData);
             myChartJS.data.labels = labels;
             myChartJS.data.datasets[0].data = periodData;
             myChartJS.update();
