8b5ac96a1d382f3e9f9e9280735135a0bfdf8236
fakharImran <fakhar.imran008@gmail.com>
2023-11-28 06:01:00 +0500

    fakhar

--- app/Imports/ImportProduct.php
+++ app/Imports/ImportProduct.php
@@ -17,48 +17,86 @@ class ImportProduct implements ToModel, WithHeadingRow
     {
         // dd($row);
         $company= Company::where('id', $row['company_id'])->first();
-        // dd($request->all());
-        $store_id = Store::select('id')->where('name_of_store', $row['store_name'])->first();
-        $category_id = Category::select('id')->where('category', $row['category_name'])->first();
-        // dd($store_id, $category_id, $row);
-        $validator = Validator::make($row, [
-            'company_id' => 'required',
-            'store_name' => 'required',
-            'category_name' => 'required',
-            'product_name' => ['required',new UniqueProductName($row['company_id'], $store_id->id??null ,$category_id->id??null)],
-            'product_number_sku' => 'required',
-            'competitor_product_name' => 'required',
-        ]);
-
-// dd($validator);
-        // dd($company->stores);
-
-        if (!$validator->fails()) {
-            // Validation failed
-            try {
-                $competitorArr= explode(',', $row['competitor_product_name']);
-                // dd($row ,json_encode($competitorArr));
-                // Process the data
-                $product = new Product([
-                    'company_id' => $row['company_id'],
-                    'store_id' => $store_id->id,
-                    'category_id' => $category_id->id,
-                    'product_name' => $row['product_name'],
-                    'product_number_sku' => $row['product_number_sku'],
-                    'competitor_product_name' => json_encode($competitorArr),
-                ]);
+        if (!($row['store_name'] == 'all' || $row['store_name'] == 'All')) {
+            $store_id = Store::select('*')->where('name_of_store', $row['store_name'])->first();
+            $category_id = Category::select('*')->where('category', $row['category_name'])->first();
+            $validator = Validator::make($row, [
+                'company_id' => 'required',
+                'store_name' => 'required',
+                'category_name' => 'required',
+                'product_name' => ['required',new UniqueProductName($row['company_id'], $store_id->id??null ,$category_id->id??null)],
+                'product_number_sku' => 'required',
+                'competitor_product_name' => 'required',
+            ]);
+            if (!$validator->fails()) {
+                // Validation failed
     
-                return $product;
-            } catch (\Throwable $e) {
-                // Handle the exception, log or display an error message
-                // For example, you can log the error using `error_log` or use Laravel's logger: \Illuminate\Support\Facades\Log::error($e);
-                // Return null or throw a custom exception, depending on your needs
-                return ;
+                try {
+                    $competitorArr= explode(',', $row['competitor_product_name']);
+                    // Process the data
+                    $product = new Product([
+                        'company_id' => $row['company_id'],
+                        'store_id' => $store_id->id,
+                        'category_id' => $category_id->id,
+                        'product_name' => $row['product_name'],
+                        'product_number_sku' => $row['product_number_sku'],
+                        'competitor_product_name' => json_encode($competitorArr),
+                    ]);
+                    $product->save();
+                    return $product;
+                } catch (\Throwable $e) {
+                    // Handle the exception, log or display an error message
+                    // For example, you can log the error using `error_log` or use Laravel's logger: \Illuminate\Support\Facades\Log::error($e);
+                    // Return null or throw a custom exception, depending on your needs
+                    return ;
+                }
             }
+            else{
+                return;
+            }
+            
         }
         else{
-            return;
+            $validator = Validator::make($row, [
+                'company_id' => 'required',
+                'store_name' => 'required',
+                'category_name' => 'required',
+                'product_name' => 'required',
+                'product_number_sku' => 'required',
+                'competitor_product_name' => 'required',
+            ]);
+            if (!$validator->fails()) {
+                $stores = Store::all();
+                $category_id = Category::select('id')->where('category', $row['category_name'])->first();
+                foreach ($stores as $key => $store) {
+                    $validator = Validator::make($row, [
+                        'product_name' => ['required',new UniqueProductName($row['company_id'], $stores[$key]->id??null ,$category_id->id??null)],
+                    ]);
+                    if (!$validator->fails()) {
+                        // Validation failed
+                        try {
+                            $competitorArr= explode(',', $row['competitor_product_name']);
+                            // Process the data
+                            $product = new Product([
+                                'company_id' => $row['company_id'],
+                                'store_id' => $stores[$key]->id,
+                                'category_id' => $category_id->id,
+                                'product_name' => $row['product_name'],
+                                'product_number_sku' => $row['product_number_sku'],
+                                'competitor_product_name' => json_encode($competitorArr),
+                            ]);
+                            $product->save();
+                        } catch (\Throwable $e) {
+                        }
+                    }
+                    else{
+                    }
+                   
+                }
+            }
+            else{
+                return;
+            }
         }
-        
     }
 }
