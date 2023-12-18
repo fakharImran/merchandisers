3980867470a73462eac655c554d827ac0b69d2fa
fakharImran <fakhar.imran008@gmail.com>
2023-12-18 23:09:00 +0500

    fakhar update checkin

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -15,34 +15,11 @@ class Activity extends Model
 {
     use HasFactory;
     protected $table= 'activities';
-    protected $fillable= ['compamy_user_id','store_location_id','store_id','category_id','product_id', 'activity_name', 'activity_detail'];
+    protected $fillable= ['compamy_user_id', 'activity_type', 'activity_description', 'activity_detail'];
 
     public function companyUser(): BelongsTo
     {
         return $this->belongsTo(CompanyUser::class, 'company_user_id');
     }
     
-    
-    public function storeLocation(): BelongsTo
-    {
-        return $this->belongsTo(StoreLocation::class, 'store_location_id');
-    }
-    
-    public function store(): BelongsTo
-    {
-        return $this->belongsTo(Store::class, 'store_id');
-    }
-   
-
-    public function category(): BelongsTo
-    {
-        return $this->belongsTo(Category::class, 'category_id');
-    }
-
-        
-    public function product(): BelongsTo
-    {
-        return $this->belongsTo(Product::class, 'product_id');
-    }
-
 }
